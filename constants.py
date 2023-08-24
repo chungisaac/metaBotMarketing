13701ab1250a4777c3da447fbd5344421e29332b
Isaac Chung <isaac.chung@clarifai.com>
2023-08-24 10:50:43 +0300

    use capitals for tag

--- constants.py
+++ constants.py
@@ -9,17 +9,17 @@ MODEL_VERSION_ID = '6c27e86364ba461d98de95cddc559cb3'
 PROMPT_TEMPLATE = Template("Prepare an example facebook post to promote Clarifai's $highlight. Only output the raw text of the example. Do not include instructions. Include the hashtags '$hashtags'. Include the relevant link $optional `$link`. Limit the entire example to 2000 characters including the link.")
 PROMOTION_HASHTAG_LINK_TUPLES = [
     ("hackathon with @lablab.ai", "#llms", "to register", "https://lablab.ai/event/llama-2-hackathon-with-clarifai"),
-    ("LLM finetuning capabilities", "#llms", "to a blogpost", "https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification"),
-    ("LLM finetuning capabilities", "#llms", "to a video tutorial", "https://www.youtube.com/watch?v=Ycl7xVA2wHk"),
-    ("vector search capabilities", "#llms", "", "https://www.clarifai.com/use-cases/visual-search"),
+    ("LLM finetuning capabilities", "#LLM", "to a blogpost", "https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification"),
+    ("LLM finetuning capabilities", "#LLM", "to a video tutorial", "https://www.youtube.com/watch?v=Ycl7xVA2wHk"),
+    ("vector search capabilities", "#LLM", "", "https://www.clarifai.com/use-cases/visual-search"),
     ("generative AI capabilities", "#generativeAI", "", "https://www.clarifai.com/products/generative-ai"),
-    ("content moderation capabilities", "#llms", "", "https://www.clarifai.com/solutions/content-moderation"),
+    ("content moderation capabilities", "#LLM", "", "https://www.clarifai.com/solutions/content-moderation"),
     ("DAM capabilities", "#Clarifai", "", "https://www.clarifai.com/solutions/digital-asset-management"),
     ("predictive maintenance capabilities", "#Clarifai", "", "https://www.clarifai.com/use-cases/predictive-maintenance"),
     ("Edge AI capabilities", "#Clarifai", "", "https://www.clarifai.com/use-cases/isr-edge-ai"),
     ("open positions", "#hiring", "", "https://www.clarifai.com/company/careers"),
-    ("LLM prompt capabilities", "#llms", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text"),
-    ("serverless inference capabilities", "#llms", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text")
+    ("LLM prompt capabilities", "#LLM", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text"),
+    ("serverless inference capabilities", "#LLM", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text")
 ]
 
 PROMPTS = [
